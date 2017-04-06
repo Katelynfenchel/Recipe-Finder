446fd567a27bf74f4e679347f18bb59d4a3ce396
Katelyn Fenchel <katelynfenchel@Katelyns-MacBook-Pro.local>
2017-04-06 14:57:26 -0500

    updated applicationcontroller

--- controllers/ApplicationController.rb
+++ controllers/ApplicationController.rb
@@ -1,27 +1,23 @@
 class ApplicationController < Sinatra::Base
-			enable :sessions
+	enable :sessions
 
-		require 'bundler'
-		Bundler.require
+	require 'bundler'
+	Bundler.require
 
-		db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/vegetarian_meals')
 
-#this query the database
-	ActiveRecord::Base.establish_connection(
-		:adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-		:database => db.path[1..-1]
 
+ActiveRecord::Base.establish_connection(
+	
+	:adapter => 'postgresql',
+	
+	:database => 'vegetarian_meals'
 	)
 
-	set :public_dir, File.expand_path('../../public', __FILE__)
-	set :views, File.expand_path('../../views', __FILE__)
-	set :session_secret, 'test'
+set :public_dir, File.expand_path('../../public', __FILE__)
+set :views, File.expand_path('../../views', __FILE__)
+set :session_secret, 'test'
 
 
-
-not_found do
-	"Not Found!"
-end
 
 
 

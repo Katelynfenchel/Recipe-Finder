0134dab21ac00a1a1eafac0365ea5d07448db98b
Katelyn Fenchel <katelynfenchel@Katelyns-MacBook-Pro.local>
2017-04-07 11:00:58 -0500

    heroku

--- controllers/ApplicationController.rb
+++ controllers/ApplicationController.rb
@@ -4,13 +4,18 @@ class ApplicationController < Sinatra::Base
 	require 'bundler'
 	Bundler.require
 
+db= URI.parse(ENV['DATABASE_URL'])
 
+DB_NAME =db.path[1..-1]
 
 ActiveRecord::Base.establish_connection(
-	
-	:adapter => 'postgresql',
-	
-	:database => 'vegetarian_meals'
+	:adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :port     => db.port,
+    :username => db.user,
+    :password => db.password,
+    :database => DB_NAME,
+    :encoding => 'utf8'
 	)
 
 set :public_dir, File.expand_path('../../public', __FILE__)

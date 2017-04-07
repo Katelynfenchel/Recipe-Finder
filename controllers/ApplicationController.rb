f334e237f41fb26cb00fe8d76391ff363918e388
Katelyn Fenchel <katelynfenchel@Katelyns-MacBook-Pro.local>
2017-04-07 12:49:38 -0500

    update

--- controllers/ApplicationController.rb
+++ controllers/ApplicationController.rb
@@ -4,19 +4,20 @@ class ApplicationController < Sinatra::Base
 	require 'bundler'
 	Bundler.require
 
-db= URI.parse(ENV['DATABASE_URL'])
 
-DB_NAME =db.path[1..-1]
+db = URI.parse(ENV['DATABASE_URL'])
 
-ActiveRecord::Base.establish_connection(
-	:adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+  DB_NAME = db.path[1..-1]
+
+  ActiveRecord::Base.establish_connection(
+    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
     :host     => db.host,
     :port     => db.port,
     :username => db.user,
     :password => db.password,
     :database => DB_NAME,
     :encoding => 'utf8'
-	)
+  )
 
 set :public_dir, File.expand_path('../../public', __FILE__)
 set :views, File.expand_path('../../views', __FILE__)

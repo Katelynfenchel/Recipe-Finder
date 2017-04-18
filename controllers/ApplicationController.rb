78116dd629c678347ca4646dc7a3e85171bb92da
Katelyn Fenchel <katelynfenchel@Katelyns-MacBook-Pro.local>
2017-04-18 11:32:42 -0500

    db matches

--- controllers/ApplicationController.rb
+++ controllers/ApplicationController.rb
@@ -1,57 +1,57 @@
-class ApplicationController < Sinatra::Base
-	enable :sessions
-
-	require 'bundler'
-	Bundler.require
-
-
-db = URI.parse(ENV['DATABASE_URL'])
-
-  DB_NAME = db.path[1..-1]
-
-  ActiveRecord::Base.establish_connection(
-    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-    :host     => db.host,
-    :port     => db.port,
-    :username => db.user,
-    :password => db.password,
-    :database => DB_NAME,
-    :encoding => 'utf8'
-  )
-
-set :public_dir, File.expand_path('../../public', __FILE__)
-set :views, File.expand_path('../../views', __FILE__)
-set :session_secret, 'test'
-
-  
-    get '/' do
-      redirect 'home/login'
-    end
-
-
-    not_found do
-      erb :not_found
-    end
-end
-
-
-
-
-
 # class ApplicationController < Sinatra::Base
-#     enable :sessions
+# 	enable :sessions
 
-#   require 'bundler'
-#   Bundler.require
+# 	require 'bundler'
+# 	Bundler.require
+
+
+# db = URI.parse(ENV['DATABASE_URL'])
+
+#   DB_NAME = db.path[1..-1]
 
 #   ActiveRecord::Base.establish_connection(
-#     :adapter => 'postgresql',
-#     :database => 'vegetarian_meals'
+#     :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+#     :host     => db.host,
+#     :port     => db.port,
+#     :username => db.user,
+#     :password => db.password,
+#     :database => DB_NAME,
+#     :encoding => 'utf8'
 #   )
 
-#   set :views, File.expand_path('../../views', __FILE__)
-#   set :public_dir, File.expand_path('../../public', __FILE__)
-#   set :session_secret, 'test'
+# set :public_dir, File.expand_path('../../public', __FILE__)
+# set :views, File.expand_path('../../views', __FILE__)
+# set :session_secret, 'test'
+
+  
+#     get '/' do
+#       redirect 'home/login'
+#     end
 
 
+#     not_found do
+#       erb :not_found
+#     end
+
+
+
+
+
+
+class ApplicationController < Sinatra::Base
+    enable :sessions
+
+  require 'bundler'
+  Bundler.require
+
+  ActiveRecord::Base.establish_connection(
+    :adapter => 'postgresql',
+    :database => 'vegetarian_meals'
+  )
+
+  set :views, File.expand_path('../../views', __FILE__)
+  set :public_dir, File.expand_path('../../public', __FILE__)
+  set :session_secret, 'test'
+
+end
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,189 +1,189 @@
 \c vegetarian_meals;
 
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
+VALUES (1,1);
+INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
+VALUES (1,2);
+INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
+VALUES (1,3);
+INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
+VALUES (1,4);
+INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
+VALUES (1,5);
+INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
+VALUES (1,6);
+INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
+VALUES (1,8);
+INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
+VALUES (1,7);
+INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
+VALUES (1,9);
+INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
+VALUES (1,10);
+INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
+VALUES (1,11);
+INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
+VALUES (1,12);
+INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
+VALUES (2,13);
+INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
+VALUES (2,8);
+INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
+VALUES (2,14);
+INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
+VALUES (2,15);
+INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
+VALUES (2,4);
+INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
 VALUES (2,1);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
 VALUES (2,2);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (2,3);
-INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (2,4);
-INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
 VALUES (2,5);
-INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (2,6);
-INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (2,8);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
 VALUES (2,7);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (2,9);
+VALUES (2,17);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (2,10);
+VALUES (2,18);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (2,11);
+VALUES (2,19);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (2,12);
+VALUES (3,20);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (3,13);
-INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (3,8);
-INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (3,14);
-INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (3,15);
+VALUES (3,21);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
 VALUES (3,4);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (3,1);
+VALUES (3,22);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (3,2);
+VALUES (3,23);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (3,5);
+VALUES (3,24);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (3,7);
+VALUES (3,25);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (3,17);
+VALUES (3,26);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (3,18);
+VALUES (3,27);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (3,19);
+VALUES (4,28);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (4,20);
+VALUES (4,7);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (4,21);
+VALUES (4,29);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
 VALUES (4,4);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (4,22);
+VALUES (4,5);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (4,23);
+VALUES (4,15);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (4,24);
+VALUES (4,9);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (4,25);
+VALUES (4,19);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (4,26);
+VALUES (4,30);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (4,27);
+VALUES (4,8);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (5,28);
+VALUES (4,31);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (5,7);
-INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (5,29);
-INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (5,4);
-INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (5,5);
-INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (5,15);
-INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (5,9);
-INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (5,19);
-INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (5,30);
+VALUES (4,32);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
 VALUES (5,8);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (5,31);
+VALUES (5,3);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (5,32);
+VALUES (5,34);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (6,8);
+VALUES (5,35);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (6,3);
+VALUES (5,36);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (6,34);
+VALUES (5,16);
+INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
+VALUES (5,4);
+INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
+VALUES (5,37);
+INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
+VALUES (5,13);
+INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
+VALUES (6,38);
+INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
+VALUES (6,39);
+INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
+VALUES (6,41);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
 VALUES (6,35);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (6,36);
+VALUES (6,13);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (6,16);
+VALUES (6,8);
+INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
+VALUES (6,7);
+INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
+VALUES (6,15);
+INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
+VALUES (6,40);
+INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
+VALUES (6,42);
+INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
+VALUES (6,24);
+INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
+VALUES (6,31);
+INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
+VALUES (6,3);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
 VALUES (6,4);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (6,37);
+VALUES (7,4);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (6,13);
-INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (7,38);
-INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (7,39);
-INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (7,41);
-INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (7,35);
-INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (7,13);
-INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (7,8);
+VALUES (7,5);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
 VALUES (7,7);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (7,15);
+VALUES (7,39);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (7,40);
+VALUES (7,44);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (7,42);
+VALUES (7,45);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (7,24);
+VALUES (8,46);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (7,31);
+VALUES (8,13);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (7,3);
-INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (7,4);
-INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (8,39);
+VALUES (8,11);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
 VALUES (8,7);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (8,4);
+VALUES (8,48);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (8,44);
+VALUES (8,49);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (8,45);
+VALUES (8,35);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (8,5);
+VALUES (8,36);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (9,50);
-INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (9,7);
+VALUES (8,34);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
 VALUES (9,4);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
 VALUES (9,5);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (13,46);
+VALUES (9,7);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (13,13);
+VALUES (9,50);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (13,11);
+VALUES (10,4);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (13,47);
-INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (13,48);
-INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (13,49);
-INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (13,35);
-INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (13,36);
-INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (13,34);
+VALUES (10,25);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
 VALUES (10,36);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
 VALUES (10,51);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
 VALUES (10,52);
-INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (10,45);
-INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (10,25);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
 VALUES (10,4);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
@@ -193,22 +193,19 @@ VALUES (11,54);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
 VALUES (11,45);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (11,44);
-INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (11,56);
-INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
 VALUES (11,20);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (11,55);
+VALUES (11,21);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
 VALUES (11,56);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
 VALUES (11,57);
+INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
+VALUES (12,4);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
 VALUES (12,45);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
 VALUES (12,58);
 INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
 VALUES (12,59);
-INSERT INTO ingredients_recipes(recipe_id, ingredient_id)  
-VALUES (12,4);
+
